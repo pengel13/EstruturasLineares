b88d44ea569cc0a43f77dfcd8c5ea6374e0571ae
Pedro Engel <130093904+pengel13@users.noreply.github.com>
2023-08-21 22:09:59 -0300

    Arquivos aula 21/08

--- exerciioossaula/src/contatos/Agenda.java
+++ exerciioossaula/src/contatos/Agenda.java
@@ -1,15 +1,43 @@
 package contatos;
 
 public class Agenda {
+	private static final int MAXIMO_ELEMENTOS = 10;
 	private Contato[] agenda;
 	private int numeroElementos = 0;
 
 	public Agenda() {
-		agenda = new Contato[10];
+		agenda = new Contato[MAXIMO_ELEMENTOS];
 		numeroElementos = 0;
 	}
 
-	public void adiciona(Contato contato) {
-		agenda[numeroElementos] = contato;
+	public boolean adiciona(Contato contato) {
+		if (numeroElementos >= 0 && numeroElementos < MAXIMO_ELEMENTOS) {
+			agenda[numeroElementos] = contato;
+			numeroElementos++;	
+			return true;
+		}
+		return false;
 	}
+
+	public Contato[] getAgenda() {
+		return agenda;
+	}
+
+	public void setAgenda(Contato[] agenda) {
+		this.agenda = agenda;
+	}
+
+	public int getNumeroElementos() {
+		return numeroElementos;
+	}
+
+	public void setNumeroElementos(int numeroElementos) {
+		this.numeroElementos = numeroElementos;
+	}
+	
+	public Contato busca(int posicao) {
+		return agenda[posicao];
+	}
+	
+	
 }
--- exerciioossaula/src/contatos/AgendaTeste.java
+++ exerciioossaula/src/contatos/AgendaTeste.java
@@ -2,13 +2,42 @@ package contatos;
 
 import contatos.Contato;
 
-
-
 public class AgendaTeste {
 
 	public static void main(String[] args) {
-		Contato c1 = new Contato("Pedro Engel", "(51) 984028208", "pedrototiengel@gmail.com",TipoContato.PESSOAL );
+		Contato c1 = new Contato("Pedro Engel", "(51) 984028208", "pedrototiengel@gmail.com", TipoContato.PESSOAL);
+		Contato c2 = new Contato("Arthur Keka", "(51) 989898524", "kellermana@gmail.com", TipoContato.PROFISSIONAL);
+		Contato c3 = new Contato("Suarez", "(51) 9999999999", "luisito@gmail.com", TipoContato.PROFISSIONAL);
+		Contato c4 = new Contato("Bagrencia", "(51) 000000000", "negge@gmail.com", TipoContato.PROFISSIONAL);
+
+		Agenda a1 = new Agenda();
+		a1.adiciona(c2);
+		a1.adiciona(c1);
+		a1.adiciona(c3);
+		a1.adiciona(c4);
+
+		Contato[] lista = a1.getAgenda();
+
+		Contato contConsultado = a1.busca(2);
+//		exibeInfo(contConsultado);
 		
+		mostraAgenda(lista);
+	}
+
+	private static void mostraAgenda(Contato[] lista) {
+		for (int index = 0; index < lista.length; index++) {
+			Contato c = lista[index];
+			if (c != null) {
+				exibeInfo(c);
+			}
+		}
+	}
+
+	private static void exibeInfo(Contato c) {
+		System.out.println("Nome:" + c.getNome());
+		System.out.println("Fone:" + c.getFone());
+		System.out.println("Email:" + c.getEmail());
+		System.out.println("Tipo de contato:" + c.getTipo().toString());
 	}
 
 }
